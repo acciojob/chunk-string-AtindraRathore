869f78a198a80249e084c7312b8edbf2a7b55de5
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-17 21:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	
+	let part1 = str.substring(0,size);
+ 	let part2 = str.substring(size+1,str.length-size-1);
+ 	let subpart1 = part2.substring(0,(size/2)+1);
+ 	let subpart2 = part2.substring((size/2)+1,part2.length);
+
+   let arr[ part1, subpart1,subpart2 ];
+	return arr;
+	
 }
 
 // Do not change the code below
